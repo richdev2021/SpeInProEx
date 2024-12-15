a76a5edc88ceabdcd64eb89bb213715f855b0423
richdev2021 <94477023+richdev2021@users.noreply.github.com>
2024-12-15 00:19:52 -0600

    enemy proyectil Implementation

--- Assets/[Scripts]/Enemys/EnemyManager.cs
+++ Assets/[Scripts]/Enemys/EnemyManager.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public GameObject[] Enemys;
-    public float Timer, TimeMax,Incrementation, direction, TotalSpeed;
-    public bool MoveDown, HaveLose, DefineTime;
+    public GameObject proyectile;
+    public float Timer, TimeMax,Incrementation, direction, TotalSpeed,ProyectilSpawnTimer,proyectilSpawnMax;
+    public int RandomEnemy, enemyCounter,RandBetfirstAndLast;
+    public bool MoveDown, HaveLose, DefineTime, noEnemysLeft;
     private void FixedUpdate()
     {
         if(HaveLose == false)
         timerFunction();
+        ProyectilSpawnTimer += 1 * Time.deltaTime;
+        if (ProyectilSpawnTimer >= proyectilSpawnMax) {
+            RandomShooting();
+            ProyectilSpawnTimer = 0;
+        }
     }
     private void timerFunction() {
         Timer += 1 * Time.deltaTime;
@@ -78,6 +85,35 @@ public class EnemyManager : MonoBehaviour
                 catch { Debug.Log("cantMove" + i); }
             }
             MoveDown = false;
+        }
+    }
+    private void RandomShooting() {
+        RandomEnemy = Random.RandomRange(0, 83);
+        for(int i = 0; i <= Enemys.Length - 1; i++) 
+        {
+            if (Enemys[i].active == true) enemyCounter += 1;
+        }
+        if (enemyCounter != 0)
+        {
+            if (Enemys[RandomEnemy].active == false) {enemyCounter = 0;
+                RandBetfirstAndLast = Random.Range(0, 2);
+                for (int i = 0; i <= Enemys.Length - 1; i++)
+                {
+                    if (RandBetfirstAndLast == 0)
+                    {
+                        if (Enemys[i].active == true) RandomEnemy = i;
+                    }
+                    else
+                    {
+                        if (Enemys[i].active == true)
+                        {
+                            RandomEnemy = i;
+                            i += 1000;
+                        }
+                    }
+                }
+            }
+            Instantiate(proyectile, Enemys[RandomEnemy].transform.position, Enemys[RandomEnemy].transform.rotation);
         }
     }
 }
---
+++ Assets/[Scripts]/proyectil/EnemyProyectilScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProyectilScript : MonoBehaviour
+{
+    public float lifeTimer = 0;
+    public float speed = 10;
+    void Update()
+    {
+        lifeTimer += 1 * Time.deltaTime;
+        transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        if (lifeTimer >= 3) Destroy(this.gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.gameObject.SetActive(false);
+            Destroy(this.gameObject);
+        }
+    }
+}
